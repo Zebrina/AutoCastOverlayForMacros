aef5bee691a423f430d9fd9e20ec8d5ff1eec76a
Sabrina <sabrina.bjurman@gmail.com>
2024-12-04 15:15:09 +0100

    code cleanup

--- AutoCastOverlayForMacros.lua
+++ AutoCastOverlayForMacros.lua
@@ -1,12 +1,12 @@
-local isDragonFlightUI = false
+local IS_RETAIL = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
+
 local BOOKTYPE_PET = BOOKTYPE_PET or Enum.SpellBookSpellBank.Pet
 local GetSpellBookItemName = GetSpellBookItemName or C_SpellBook.GetSpellBookItemName
 
--- Wrapper for GetActionInfo to make it behave in classic as it does in retail.
-local function GetActionInfo(slot)
-	if (isDragonFlightUI) then
-		return _G.GetActionInfo(slot)
-	else
+local GetActionInfo = _G.GetActionInfo
+if (not IS_RETAIL) then
+	-- Wrapper for GetActionInfo to make it behave in classic as it does in retail.
+	GetActionInfo = function(slot)
 		local actionType, id = _G.GetActionInfo(slot)
 		return actionType, GetMacroSpell(id), nil
 	end
@@ -29,18 +29,28 @@ local function GetPetSpellBookSlot(spellNameOrID)
 	return nil
 end
 
-local function ActionButton_ShowAutoCastOverlay(button, show)
-	if (isDragonFlightUI) then
+local ActionButton_ShowAutoCastOverlay
+if (IS_RETAIL) then
+	-- Retail
+	ActionButton_ShowAutoCastOverlay = function(button, show)
 		button.AutoCastOverlay:SetShown(show)
-	else
+	end
+else
+	-- Classic
+	ActionButton_ShowAutoCastOverlay = function(button, show)
 		button.AutoCastable:SetShown(show)
 	end
 end
 
-local function ActionButton_ShowAutoCastEnabled(button, isEnabled)
-	if (isDragonFlightUI) then
+local ActionButton_ShowAutoCastEnabled
+if (IS_RETAIL) then
+	-- Retail
+	ActionButton_ShowAutoCastEnabled = function(button, isEnabled)
 		button.AutoCastOverlay:ShowAutoCastEnabled(isEnabled)
-	else
+	end
+else
+	-- Classic
+	ActionButton_ShowAutoCastEnabled = function(button, isEnabled)
 		button.AutoCastShine:SetShown(isEnabled)
 		if (isEnabled) then
 			AutoCastShine_AutoCastStart(button.AutoCastShine)
@@ -65,27 +75,23 @@ local function ActionButton_UpdateFlash(button)
 	end
 end
 
-do
-    local _, _, _, tocVersion = GetBuildInfo()
-    if (tocVersion >= 90000) then
-		isDragonFlightUI = (tocVersion >= 100000)
-
-		local buttonNames = {
-			"ActionButton",
-			"MultiBarBottomLeftButton",
-			"MultiBarBottomRightButton",
-			"MultiBarLeftButton",
-			"MultiBarRightButton",
-			"MultiBar5Button",
-			"MultiBar6Button",
-			"MultiBar7Button",
-		}
-		for i = 1, #buttonNames do
-			for j = 1, NUM_ACTIONBAR_BUTTONS do
-				hooksecurefunc(_G[buttonNames[i]..j], "UpdateFlash", ActionButton_UpdateFlash)
-			end
+-- Initialization
+if (IS_RETAIL) then
+	local buttonNames = {
+		"ActionButton",
+		"MultiBarBottomLeftButton",
+		"MultiBarBottomRightButton",
+		"MultiBarLeftButton",
+		"MultiBarRightButton",
+		"MultiBar5Button",
+		"MultiBar6Button",
+		"MultiBar7Button",
+	}
+	for i = 1, #buttonNames do
+		for j = 1, NUM_ACTIONBAR_BUTTONS do
+			hooksecurefunc(_G[buttonNames[i]..j], "UpdateFlash", ActionButton_UpdateFlash)
 		end
-    else
-        hooksecurefunc("ActionButton_UpdateFlash", ActionButton_UpdateFlash)
-    end
+	end
+else
+	hooksecurefunc("ActionButton_UpdateFlash", ActionButton_UpdateFlash)
 end
